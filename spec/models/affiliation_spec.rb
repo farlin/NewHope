50703796267486a2a312814dccf435843b06b89c
obscurus <farlin@gmail.com>
2022-07-15 00:57:34 +1000

    basic model tests

--- spec/models/affiliation_spec.rb
+++ spec/models/affiliation_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Affiliation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has none to begin with" do
+    expect(Affiliation.count).to eq 0
+  end
+
+  it "has one after adding one" do
+    create (:affiliation)
+    expect(Affiliation.count).to eq 1
+  end
+
+  it "has none after one was created in a previous example" do
+    expect(Affiliation.count).to eq 0
+  end
+
+
+  it "not valid without name" do
+    res = build(:affiliation, name: " ")
+    expect( res.valid? ).to eq false
+  end
+
+  it "accepts unique name" do
+    create(:affiliation, name: "Gaiya")
+    res = build(:affiliation, name: "Gaiya")
+
+    expect( res.valid? ).to eq false
+  end
+
+  it "doesn't accept case insensitive name" do
+    create(:affiliation, name: "Gaiya")
+    res = build(:affiliation, name: "GaiYa")
+
+    expect( res.valid? ).to eq false
+  end
+
 end
